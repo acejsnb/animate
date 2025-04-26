66628018fa07891727bc6dbdb3a45052800decf3
ace <xsmails@163.com>
2025-04-26 23:46:29 +0800

    feat: init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/component/header'
+import Main from '@/component/main'
+import Footer from '@/component/footer'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
